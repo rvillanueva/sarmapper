7ef747c1e7c583b15d191965e0c59818e4e87c6e
Ryan Villanueva <ryanvill@gmail.com>
2026-02-13 04:55:24 +0000

    convert to ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./tools/setupTests.js",
+    setupFiles: "./tools/setupTests.ts",
     exclude: [
       "**/node_modules/**",
       "**/dist/**",
