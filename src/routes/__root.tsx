2ba1b5b1d638bd722eeb31eb95a221f30c8d56a3
Ryan Villanueva <ryanvill@gmail.com>
2026-02-13 05:14:02 +0000

    add oxlint

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,8 +2,6 @@ import { HeadContent, Scripts, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import { TanStackDevtools } from "@tanstack/react-devtools";
 
-import Header from "../components/Header";
-
 import appCss from "../styles.css?url";
 
 export const Route = createRootRoute({
@@ -17,7 +15,7 @@ export const Route = createRootRoute({
         content: "width=device-width, initial-scale=1",
       },
       {
-        title: "TanStack Start Starter",
+        title: "SAR Mapper",
       },
     ],
     links: [
@@ -37,7 +35,6 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <HeadContent />
       </head>
       <body>
-        <Header />
         {children}
         <TanStackDevtools
           config={{
