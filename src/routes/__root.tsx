4830ea4feb67be61563377d7ac6549fed8ec3921
Ryan Villanueva <ryanvill@gmail.com>
2026-02-13 15:32:34 +0000

    Add favicon and apple-touch-icon links to root route (#46)

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -17,11 +17,39 @@ export const Route = createRootRoute({
       {
         title: "SAR Mapper",
       },
+      {
+        name: "apple-mobile-web-app-title",
+        content: "SAR Mapper",
+      },
     ],
     links: [
       {
         rel: "stylesheet",
         href: appCss,
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        href: "/favicon-96x96.png",
+        sizes: "96x96",
+      },
+      {
+        rel: "icon",
+        type: "image/svg+xml",
+        href: "/favicon.svg",
+      },
+      {
+        rel: "shortcut icon",
+        href: "/favicon.ico",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
+      },
+      {
+        rel: "manifest",
+        href: "/site.webmanifest",
       },
     ],
   }),
